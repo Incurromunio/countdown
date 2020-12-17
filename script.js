5a57a155e1cc63135ccec945af2209c7c09d6ccd
Incurromunio <75810561+Incurromunio@users.noreply.github.com>
2020-12-17 18:33:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEL = document.getElementById('hours');
 const minsEL = document.getElementById('mins');
 const secondsEL = document.getElementById('seconds');
 
-const newYears = "17 Dec 2020 18:30";
+const newYears = "1 Jan 2021";
 
 function countdown() {
   const newYearsDate = new Date(newYears);
